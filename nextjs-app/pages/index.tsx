e4d722da973f9bb55d9c026bfbea7e42f9016043
jimmy-kyalo <kyalo90@gmail.com>
2023-05-30 13:49:36 +0300

    controllers

--- nextjs-app/pages/index.tsx
+++ nextjs-app/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Link from "next/link";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -107,6 +108,9 @@ export default function Home() {
               with&nbsp;Vercel.
             </p>
           </a>
+          <div>
+            <Link href={"/hello"}>Go to hello page</Link>
+          </div>
         </div>
       </main>
     </>
